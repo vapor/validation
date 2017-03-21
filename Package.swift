d5eab16f18d36cec808302f58a3a271328bfcb27
Logan Wright <logan.william.wright@gmail.com>
2017-03-21 18:52:44 +0100

    validation in isolated package

---
+++ Package.swift
@@ -0,0 +1,8 @@
+import PackageDescription
+
+let package = Package(
+    name: "Validation",
+    dependencies: [
+        .Package(url: "https://github.com/vapor/debugging.git", majorVersion: 0),
+    ]
+)
